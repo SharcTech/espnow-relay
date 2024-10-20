526f7db1eaf35e02b3416bcd91703af62d14bbc8
ChrisMisztur <cmisztur@mriiot.com>
2024-10-19 22:34:08 -0500

    no message

--- Tasks/MoveToBrokerTask.py
+++ Tasks/MoveToBrokerTask.py
@@ -93,9 +93,9 @@ class MoveToBrokerTask:
                             }
 
                         await client.publish(message_topic, json.dumps(message_payload), retain=message_retain)
-                        self._logger.debug("[broker] sent topic:%s, payload:%s",message_topic, message_payload)
+                        self._logger.info("[broker] sent topic:%s, payload:%s",message_topic, message_payload)
                     except:
-                        self._logger.warn("[broker] failed parser, message:%s", message['message'])
+                        self._logger.warning("[broker] failed parser, message:%s", message['message'])
 
         except MqttError as e:
             self._logger.error("[broker] failed")
